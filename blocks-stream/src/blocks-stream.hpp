3cb4e1198c22ef126f6e94f74bc1b90f5dbe50f6
yakud <yakudgm@gmail.com>
2019-11-14 17:48:58 +0300

    Fix validator build

--- blocks-stream/src/blocks-stream.hpp
+++ blocks-stream/src/blocks-stream.hpp
@@ -22,12 +22,65 @@ public:
         return instance;
     };
 
-    bool Init(const std::string& filename);
-    BlocksStream() = default;
-    ~BlocksStream();
+    bool Init(const std::string& filename) {
+        outfileIndex.open(filename + ".index", std::ofstream::out | std::ofstream::app | std::ofstream::ate | std::ofstream::binary | std::ios_base::app) ;
+        outfile.open(filename, std::ofstream::out | std::ofstream::app | std::ofstream::ate | std::ofstream::binary | std::ios_base::app) ;
+        return true;
+    }
 
-    bool write_block(const std::string &stream);
-    void writer();
+    BlocksStream() = default;
+    ~BlocksStream() {
+        if (outfile.is_open())
+            outfile.close();
+
+        if (outfileIndex)
+            outfileIndex.close();
+
+        delete(blocksQueue);
+    }
+
+    bool write_block(const std::string &stream) {
+        blocksQueue->push(stream);
+        return true;
+    }
+    void writer() {
+        // todo: params
+        BlockingQueue<std::string> bq(10000);
+        blocksQueue = &bq;
+
+        std::ostringstream header_buffer;
+        std::ostringstream body_buffer;
+        std::string nextMessage;
+
+        // todo: graceful shutdown
+        while (true) {
+            if (need_stop) {
+                break;
+            }
+            if (!blocksQueue->pop(nextMessage)) {
+                if (blocksQueue->closed()) {
+                    break;
+                }
+                continue;
+            }
+
+            body_buffer << nextMessage;
+
+            auto num = uint32_t(nextMessage.length());
+            header_buffer.write(reinterpret_cast<const char *>(&num), sizeof(num));
+
+            outfile << body_buffer.str();
+            outfile.flush();
+            outfileIndex << header_buffer.str();
+            outfileIndex.flush();
+
+            body_buffer.str("");
+            body_buffer.clear();
+            header_buffer.str("");
+            header_buffer.clear();
+            nextMessage.clear();
+        }
+    }
 
 protected:
     BlockingQueue<std::string> *blocksQueue{};
